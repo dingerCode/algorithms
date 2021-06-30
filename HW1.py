ed453b4568be0c75332bb0463a8c8b6addcee7dd
dingerCode <artiaga11@gmail.com>
2021-06-29 22:24:49 -0500

    HW2 Completed

--- HW1.py
+++ HW1.py
@@ -2,36 +2,43 @@
 # n, find the sum of digits in all numbers from 1 to n.
 # Example: n = 5. Result = 1 + 2 + 3 + 4 + 5 = 15
 def sumOfDigits(num):
-    result = 0
-    if num != 0:
-        for i in range(1, num + 1):
-            result += i
-    print(result)
+    return (num * (num + 1)) / 2
 
-sumOfDigits(5)
+print(sumOfDigits(5))
+
 
 # 2. Find max number from 3 values, entered manually from a keyboard.
 # Example: 124, 21, 32. Result = 124.
 def maxNum(n1, n2, n3):
-    highest = max(n1, n2, n3)
-    print(highest)
+    return max(n1, n2, n3)
 
-maxNum(124, 21, 32)
+# without built-in functions
+def max_num(a, b, c):
+    if a > b and a > c:
+        return a
+    if b > a and b > c:
+        return
+    return c
+
+print(maxNum(124, 21, 32))
+print(max_num(124, 21, 32))
+
 
 # 3. Count odd and even numbers. Count odd and even digits of the whole number.
 # Example: entered number is 34560, then 3 digits will be even (4, 6, and 0) and 2 odd
 # digits (3 and 5).
-def oddEven(n):
-    evenNums = []
-    oddNums = []
-    while (n > 0):
-        rem = n % 10
-        if (rem % 2 == 0):
-            evenNums.append(rem)
-        else:
-            oddNums.append(rem)
-        n = int(n / 10)
-    print("Even Numbers : ", evenNums)
-    print("Odd Numbers : ", oddNums)
+def oddEvenCount(n):
+    odds = 0
+    evens = 0
 
-oddEven(34560)
+    while n != 0:
+        current_digit = n % 10
+        if current_digit % 2:
+            odds += 1
+        else:
+            evens += 1
+        n = n // 10
+
+    return ["Evens: " + str(evens), "Odds: " + str(odds)]
+
+print(oddEvenCount(34560))
---
+++ HW2.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+# 1. Split in Half
+# Given a string. Split it into two equal parts. Swap these parts and return the result.
+# If the string has odd characters, the first part should be one character greater than the
+# second part.
+# Example: string = 'bbbbbcaaaaa'. Result = ‘aaaaabbbbbc’.
+
+def splitInHalf(str):
+    dividedLength = round(len(str)/2)
+    str1 = str[:dividedLength]
+    str2 = str[dividedLength:]
+    return str2 + str1
+
+
+print(splitInHalf("bbbbbcaaaaa"))
+
+
+# 2. Unique Characters in String.
+# Given a string, determine if it consists of all unique characters.
+# For example, the string 'abcde' has all unique characters and should return True.
+# The string 'aabcde' contains duplicate characters and should return False.
+
+def uniqueChars(str):
+    count = Counter(str)
+    if (len(count) == len(str)):
+        return True
+    else:
+        return False
+
+print(uniqueChars('abcde'))
+print(uniqueChars(('aabcde')))
