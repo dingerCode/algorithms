7ac1b371198df68b8a750dad688ea732762c218f
dingerCode <artiaga11@gmail.com>
2021-07-03 23:43:11 -0500

    HW3 Completed

---
+++ HW3.py
@@ -0,0 +1,44 @@
+from itertools import permutations
+
+# 1. Reverse a Statement
+# Build an algorithm that will print the given statement in reverse.
+# Example: Initial string = Everything is hard before it is easy
+# Reversed string = easy is it before hard is Everything
+
+def stringReversal(s):
+    myList = s.split(" ")
+    rList = myList[::-1]
+    return " ".join(rList)
+
+
+print(stringReversal('Everything is hard before it is easy'))
+
+# 2. Permutations
+# Write a solution that will print all permutations of a string.
+# A permutation is an act of changing the arrangement of characters.
+# Example: String = ABC, Return {ABC, ACB, BAC, BCA, CBA, CAB}
+
+def strPerm(s):
+    perm = [''.join(p) for p in permutations(s)]
+    return perm
+
+
+print(strPerm('ABC'))
+
+# 3. Count Characters
+# Create a program that will count vowels and consonants in a string.
+# Example: String = “hello world”, Return {Vowels: 3, Consonants: 7}
+
+def countChars(s):
+    vowels = 0
+    consonants = 0
+    lowerStr = s.lower()
+    for i in lowerStr:
+        if (i == 'a' or i == 'e' or i == 'i' or i == 'o' or i == 'u'):
+            vowels += 1
+        elif (i != " "):
+            consonants += 1
+    print('Vowels: ' + str(vowels) + ' Consonants: ' + str(consonants))
+
+
+countChars("hello world")
